8d0e1bbe2979d1b5ef7a4a3c5cfbc6bdc7bc8ae7
Stork1323 <tamd132003@gmail.com>
2023-11-16 14:07:16 +0700

    updated 16/11

--- src/motor.c
+++ src/motor.c
@@ -3,10 +3,10 @@
 #include <util/delay.h>
 #include <motor.h>
 
-#define PWM1_PIN 1
-#define PWM2_PIN 2
-#define IN1 3
-#define IN3 4
+#define PWM1_PIN 3 //pd3
+#define PWM2_PIN 3 //pb3
+#define IN1 4
+#define IN3 5
 #define role_pin 0
 //#define uint8_t unsigned char 
 typedef unsigned char uint8_t;
@@ -51,15 +51,16 @@ void role_off()
 }
 void motor_init()
 {
-    DDRB |= (1<< PWM1_PIN) | (1<< PWM2_PIN); // PWM1_PIN = IN2, PWM2_PIN = IN4 
+    DDRD |= (1<< PWM1_PIN); // PWM1_PIN = IN2, PWM2_PIN = IN4 
+    DDRB |= (1<< PWM2_PIN);
     DDRB |= (1 << IN1) | (1 << IN3);
     PORTB &= ~((1 << IN1) | (1 << IN3));
 }
 void motor_run(unsigned char speed1, unsigned char speed2)
 {
-    OCR1A = speed1;
-    OCR1B = speed2;
-    TCCR1A |= (1<< WGM10) | (1<< COM1A1) | (1<< COM1B1);
-    TCCR1B |= (1<< WGM12) | (1<< CS11) | (1<< CS10); // che do FPWM top la 0CR1A
+    OCR2A = speed1;
+    OCR2B = speed2;
+    TCCR2A |= (1<< WGM20) | (1<< COM2A1) | (1<< COM2B1) | (1<< WGM21);
+    TCCR2B |= (1<< CS22) | (1<< CS20); // che do FPWM top la 0CR2A
 
 }
